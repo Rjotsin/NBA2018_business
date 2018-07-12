9deb9074846abab5068dd1c14f85c058561924fa
rishi <rjotsin@gmail.com>
2018-07-12 09:42:41 -0400

    added back logistic
    also added rfe

--- nba_business.py
+++ nba_business.py
@@ -14,7 +14,7 @@ np.random.seed(1)
 
 views = pd.read_csv(r'training_set.csv')
 views = views.pivot(index='Game_ID', columns='Country', values='Rounded Viewers')
-views = views/100000
+views = views
 views = views.fillna(0)
 #views.to_csv('views.csv')
 
@@ -24,8 +24,12 @@ player = pd.read_csv(r'player_data.csv')
 #.transform(lambda x: (x!='None')
 
 game = pd.read_csv(r'game_data.csv')
+streaks = pd.read_csv(r'streaks.csv')
+streaks['result'] = np.where(streaks['result']=='f', 0,1)
 
-game1 = pd.pivot_table(game,index=['Game_ID'], columns=['Location'], values=['Game_Date', 'Season', 'Team'], aggfunc=sum)
+game_streaks=pd.merge(game,streaks,left_on=['Game_ID', 'Location'], right_on=['game_id', 'location'] , how='left')
+
+game1 = pd.pivot_table(game_streaks,index=['Game_ID'], columns=['Location'], values=['Game_Date', 'Season', 'Team'], aggfunc=sum)
 
 #game1['Game_ID'] = game1.index
 game1.columns = ['Game_Date','Game_Date_2','Season','Season_2','Team_A','Team_H']
@@ -33,23 +37,30 @@ game1 = game1.drop('Game_Date_2',1)
 game1 = game1.drop('Season_2',1)
 
 
-game2 = pd.pivot_table(game,index=['Game_ID'], columns=['Location'], values=['Wins_Entering_Gm', \
+game2 = pd.pivot_table(game_streaks,index=['Game_ID'], columns=['Location'], values=['Wins_Entering_Gm', \
                        'Losses_Entering_Gm', 'Team_Minutes', 'Final_Score', 'Lead_Changes', \
                        'Ties', 'Largest_Lead', 'Full_Timeouts','Short_Timeouts', 'Qtr_4_Score', \
-                       'L2M_Score'], aggfunc=np.mean)
+                       'L2M_Score','streak_win_before', 'streak_loss_before','result'], aggfunc=np.mean)
 #game2['Game_ID'] = game2.index
 game2.columns = ['Final_Score_A', 'Final_Score_H', 'Full_Timeouts_A', 'Full_Timeouts_H', 'L2M_Score_A', \
                  'L2M_Score_H', 'Largest_Lead_A', 'Largest_Lead_H', 'Lead_Changes_A', 'Lead_Changes_H', \
                   'Losses_Entering_Gm_A', 'Losses_Entering_Gm_H', 'Qtr_4_Score_A', 'Qtr_4_Score_H', \
                   'Short_Timeouts_A', 'Short_Timeouts_H', 'Team_Minutes_A', 'Team_Minutes_H', 'Ties_A', \
-                  'Ties_H', 'Wins_Entering_Gm_A', 'Wins_Entering_Gm_H']
+                  'Ties_H', 'Wins_Entering_Gm_A', 'Wins_Entering_Gm_H','result_A','result_H','streak_loss_before_A',\
+                 'streak_loss_before_H', 'streak_win_before_A','streak_win_before_H']
 
+game2['max_win'] = game2[['streak_win_before_A','streak_win_before_H']].max(axis=1).fillna(0)
+game2['max_loss'] = game2[['streak_loss_before_A','streak_loss_before_H']].max(axis=1).fillna(0)
+game2 = game2.drop(['streak_loss_before_A','streak_loss_before_H', 'streak_win_before_A','streak_win_before_H'],1)
+        
 dfgame=pd.merge(game1,game2,on='Game_ID', how='inner')
 
 dfgame['Game_Date'] = pd.to_datetime(dfgame['Game_Date'])
 dfyear = pd.get_dummies('y' + dfgame['Game_Date'].dt.strftime('%y'))
 dfmonth = pd.get_dummies('m' + dfgame['Game_Date'].dt.strftime('%m'))
-dfday = pd.get_dummies('d' + dfgame['Game_Date'].dt.strftime('%d'))
+dfday = pd.get_dummies('d' + dfgame['Game_Date'].dt.dayofweek.apply(str))
+#dfwin = pd.get_dummies('w' + dfgame['max_win'].apply(str))
+#dfloss = pd.get_dummies('l' + dfgame['max_loss'].apply(str))
 dfgame['Season_Num'] = np.where(dfgame['Season']=='2016-17', 0,1)
 
 dfgame['Team_A'] = dfgame['Team_A'] + '_A'
@@ -62,13 +73,19 @@ dfgame = dfgame.join(dfTeamA)
 dfgame = dfgame.join(dfTeamH)
 dfgame = dfgame.drop('Team_A',1)
 dfgame = dfgame.drop('Team_H',1)
+#dfgame = dfgame.join(dfwin)
+#dfgame = dfgame.join(dfloss)
+#dfgame = dfgame.drop('max_win',1)
+#dfgame = dfgame.drop('max_loss',1)
 dfgame = dfgame.join(dfyear)
 dfgame = dfgame.join(dfmonth)
 dfgame = dfgame.join(dfday)
 dfgame.to_csv('gametst.csv')
-gp =dfgame.columns[25:85].tolist()
-gp_A =dfgame.columns[24:55].tolist() + dfgame.columns[85:95].tolist()
-gp_H =dfgame.columns[24:25].tolist() + dfgame.columns[55:95].tolist()
+gp =dfgame.columns[29:89].tolist()
+gp_A =dfgame.columns[28:59].tolist() + dfgame.columns[89:106].tolist()
+gp_H =dfgame.columns[28:29].tolist() + dfgame.columns[59:106].tolist()
+gp_A2 =dfgame.columns[28:59].tolist() + dfgame.columns[89:99].tolist()
+gp_H2 =dfgame.columns[28:29].tolist() + dfgame.columns[59:99].tolist() #+ dfgame.columns[127:137].tolist()
 gp_for = game.columns[7:].tolist()
 
 for i in gp_for:    
@@ -76,37 +93,70 @@ for i in gp_for:
     dfgame[i + '_H'] = dfgame.groupby(gp)[i + '_H'].transform(lambda x: x.fillna(x.mean()))
     dfgame[i + '_A'] = dfgame.groupby(gp_A)[i + '_A'].transform(lambda x: x.fillna(x.mean()))
     dfgame[i + '_H'] = dfgame.groupby(gp_H)[i + '_H'].transform(lambda x: x.fillna(x.mean()))
+    dfgame[i + '_A'] = dfgame.groupby(gp_A2)[i + '_A'].transform(lambda x: x.fillna(x.mean()))
+    dfgame[i + '_H'] = dfgame.groupby(gp_H2)[i + '_H'].transform(lambda x: x.fillna(x.mean()))
 
 dfgame.to_csv('gametst.csv')
 
 
+dfTeam = dfgame.iloc[:,29:89].T.groupby([s.split('_')[0] for s in dfgame.iloc[:,29:89].T.index.values]).sum().T
 
-
-dfTeam = dfgame.iloc[:,25:85].T.groupby([s.split('_')[0] for s in dfgame.iloc[:,25:85].T.index.values]).sum().T
-
-df = pd.merge(dfgame.iloc[:,:25], dfTeam, on='Game_ID', how='inner')
-df2 = pd.merge(df, dfgame.iloc[:,85:], on='Game_ID', how='inner')
+df = pd.merge(dfgame.iloc[:,:29], dfTeam, on='Game_ID', how='inner')
+df2 = pd.merge(df, dfgame.iloc[:,89:], on='Game_ID', how='inner')
 df3 = pd.merge(df2, views, on='Game_ID', how='inner')
 df3.rename(columns={'Team_Minutes_A':'Team_Minutes'}, inplace=True)
 df3 = df3.drop('Team_Minutes_H',1)
 
 #df3.to_csv('output.csv')
 
-train_cols = df3.columns[2:95] #64
+train_cols = df3.columns[2:75] #99-31 = 68 + 7 = 75
 views_for = views.columns.tolist()
 
+cols = {}
 
-ols={}
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+rfe = RFE(logreg, 20)
+for k in views_for:
+    cols['rfe' + k] = rfe.fit(df3[train_cols], df3[k])
+
+
+
+train_cols2 = df3.columns[2:27].union(df3.columns[28:68])
+train_cols3 = df3.columns[2:58]
+train_cols4 = df3.columns[12:58]
+
+
+logit={}
 result = {}
 for i in views_for:
     try:
         df4 = df3.loc[df3[i] != 0]
-        df4 = df4[train_cols]
-        train_for = df4.loc[:, (df4 != 0).any(axis=0)]
+        train_for = df4[train_cols].apply(np.log)
         result['r_' + i] = train_for.columns[:]
-        ols['l_' + i] = sm.OLS(df3[i], df3[train_for.columns[:]]).fit()
+        logit['l_' + i] = sm.Logit(df3[i], df3[train_for.columns[:]]).fit()
     except:
-        pass
+        try:
+            df4 = df3.loc[df3[i] != 0]
+            df4 = df4[train_cols2]
+            train_for = df4.loc[:, (df4 != 0).any(axis=0)]
+            result['r_' + i] = train_for.columns[:]
+            logit['l_' + i] = sm.Logit(df3[i], df3[train_for.columns[:]]).fit()
+        except:
+            try:
+                df4 = df3.loc[df3[i] != 0]
+                df4 = df4[train_cols3]
+                train_for = df4.loc[:, (df4 != 0).any(axis=0)]
+                result['r_' + i] = train_for.columns[:]
+                logit['l_' + i] = sm.Logit(df3[i], df3[train_for.columns[:]]).fit()
+            except:
+                df4 = df3.loc[df3[i] != 0]
+                df4 = df4[train_cols4]
+                train_for = df4.loc[:, (df4 != 0).any(axis=0)]
+                result['r_' + i] = train_for.columns[:]
+                logit['l_' + i] = sm.Logit(df3[i], df3[train_for.columns[:]]).fit()
     
 views_test = pd.read_csv(r'test_set.csv')
 test2 = pd.merge(df2, views_test, on='Game_ID', how='inner')
@@ -116,7 +166,7 @@ test2 = test2.drop('Team_Minutes_H',1)
 test_country = pd.DataFrame()
 test_country['Game_ID'] = views_test['Game_ID']
 for j in views_for:
-    test_country[j] =ols['l_' + j].predict(test2[result['r_' + j]])
+    test_country[j] =logit['l_' + j].predict(test2[result['r_' + j]])
 test_country['Total_Viewers'] = test_country.sum(axis=1)
 test_country['Total_Viewers'] = test_country['Total_Viewers'] - test_country['Game_ID']
 
